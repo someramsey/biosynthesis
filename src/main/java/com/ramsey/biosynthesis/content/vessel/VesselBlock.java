f782dee0f57752943b8e7b34ef542dc87824614d
Ramsey <someramseyy@gmail.com>
2024-07-12 17:06:55 +0200

    Fixed highligth shape

--- src/main/java/com/ramsey/biosynthesis/content/vessel/VesselBlock.java
+++ src/main/java/com/ramsey/biosynthesis/content/vessel/VesselBlock.java
@@ -1,16 +1,25 @@
 package com.ramsey.biosynthesis.content.vessel;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class VesselBlock extends BaseEntityBlock {
     public VesselBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public @NotNull VoxelShape getShape(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
+        return Shapes.box(0.25, 0, 0.25, 0.75, 0.5, 0.75);
     }
 
     @Override
